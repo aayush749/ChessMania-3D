122eaffc4c2e03aef6782a5508a121629b19689c
Aayush Anand <aaayushanand5@gmail.com>
2022-06-20 13:47:59 +0530

    Added Scene Management to main screen

---
+++ Assets/Scripts/ScreenLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ScreenLoader : MonoBehaviour
+{
+    /**
+     * Loads the main game screen. Uses "MainGame" string name to load the screen. Update this name if scene name is changed. 
+     */
+    public void LoadMainGameScreen()
+    {
+        Debug.Log("Loading Screen: MainScreen");
+        SceneManager.LoadScene("MainGame");
+    }
+
+    /**
+     * Quit the game
+     */
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game!");
+        Application.Quit();
+    }
+}
